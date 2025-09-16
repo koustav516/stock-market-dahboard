53f4f45ba5b7626f6ba6cd5043a0509935ce164e
koustav516 <koustav516@gmail.com>
2025-09-15 23:14:08 -0500

    Added Chart in dashboard

---
+++ src/components/Chart.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { mockHistoricalData } from "../constants/mock";
+import Card from "./Card";
+import { convertUnixTimeStampToDate } from "../helpers/date-helper";
+import {
+    Area,
+    AreaChart,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+} from "recharts";
+import { chartConfig } from "../constants/config";
+import ChartFilter from "./ChartFilter";
+
+const Chart = () => {
+    const [data, setData] = useState(mockHistoricalData);
+    const [filter, setFilter] = useState("1W");
+
+    const formData = () => {
+        return data.c.map((item, index) => {
+            return {
+                value: item.toFixed(2),
+                date: convertUnixTimeStampToDate(data.t[index]),
+            };
+        });
+    };
+
+    return (
+        <Card>
+            <ul className="flex absolute top-2 right-2 z-40">
+                {Object.keys(chartConfig).map((item) => {
+                    return (
+                        <li key={item}>
+                            <ChartFilter
+                                text={item}
+                                active={filter === item}
+                                onClick={() => {
+                                    setFilter(item);
+                                }}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+            <ResponsiveContainer>
+                <AreaChart data={formData(data)}>
+                    <defs>
+                        <linearGradient
+                            id="chartColor"
+                            x1="0"
+                            y1="0"
+                            x2="0"
+                            y2="1"
+                        >
+                            <stop
+                                offset="5%"
+                                stopColor="rgb(199,210,254)"
+                                stopOpacity={0.8}
+                            />
+                            <stop
+                                offset="95%"
+                                stopColor="rgb(199,210,254)"
+                                stopOpacity={0}
+                            />
+                        </linearGradient>
+                    </defs>
+                    <Area
+                        type="monotone"
+                        dataKey="value"
+                        stroke="#312e81"
+                        fillOpacity={1}
+                        strokeWidth={0.5}
+                        fill="url(#chartColor)"
+                    />
+                    <Tooltip />
+                    <XAxis dataKey={"date"} />
+                    <YAxis domain={["dataMin", "dataMax"]} />
+                </AreaChart>
+            </ResponsiveContainer>
+        </Card>
+    );
+};
+
+export default Chart;
