b622666c9092b250ceb5fbcf2da61635905637fd
koustav516 <koustav516@gmail.com>
2025-09-15 19:02:36 -0500

    Added basic configs

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Card from "./Card";
+
+const Dashboard = () => {
+    return;
+};
+
+export default Dashboard;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            colors: {
+                white: "white",
+                none: "none",
+            },
+            borderWidth: {
+                1: "1px",
+            },
+            fontFamily: {
+                quicksand: ["Quicksand", "sans-sarif"],
+            },
+            gridTemplateRows: {
+                7: "repeat(7,minmax(0,1fr))",
+                8: "repeat(8,minmax(0,1fr))",
+            },
+        },
+    },
+    plugins: [],
+};
